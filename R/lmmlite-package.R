4ff481f4b3863ce066bfab8c817ff8f44f4e3a28
Karl Broman <kbroman@gmail.com>
2026-06-01 11:58:37 -0500

    Add links to vignettes in lmmlite-package.Rd

--- R/lmmlite-package.R
+++ R/lmmlite-package.R
@@ -1,2 +1,12 @@
 #' @keywords internal
+#' @section Vignettes:
+#' - [User guide](https://kbroman.org/lmmlite/assets/lmmlite.html)
+#'
+#' - [Results compared](https://kbroman.org/lmmlite/assets/compare2pylmm.html)
+#'   to [pylmm](https://github.com/nickFurlotte/pylmm) (and also the R package
+#'   [regress](https://cran.r-project.org/package=regress)).
+#'
+#' - [Performance compared](https://kbroman.org/lmmlite/assets/performance.html)
+#'   to pylmm.
+#'
 "_PACKAGE"
